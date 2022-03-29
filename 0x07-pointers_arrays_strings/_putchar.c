d42f5be5715589ee0818ac15cca44724e7cd1895
Vicky2354 <ukpongvictory77@gmail.com>
2022-03-29 06:08:05 -0700

    _putchar.c

--- 0x07-pointers_arrays_strings/_putchar.c
+++ 0x07-pointers_arrays_strings/_putchar.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
 /**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- * On error, -1 is returned, and errno is set appropriately.
- * Return: On success 1.
+ * _putchar - function prints a char
+ * @c: is a character to be printed
+ * Return: is a character.
  */
 
 char _putchar(char c)
 {
-	return (write(1, &c, 1))
+	putchar(c);
+	return (c);
 }
